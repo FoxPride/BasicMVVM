256b516dd582d8f509d005035c70dd6054579148
andbayd <andrew@monosnap.com>
2021-06-12 19:32:06 +0300

    IoC

--- DefaultMVVM/App.xaml.cs
+++ DefaultMVVM/App.xaml.cs
@@ -1,6 +1,36 @@
-﻿namespace DefaultMVVM
+﻿using System.Windows;
+using DefaultLibrary.Helpers;
+using DefaultMVVM.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+
+namespace DefaultMVVM
 {
-    public partial class App
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default <see cref="Application"/> class.
+    /// </summary>
+    sealed partial class App
     {
+        /// <summary>
+        /// Initializes the singleton application object. This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                    .RegisterViewModels()
+                    .RegisterServices()
+                    .BuildServiceProvider());
+
+            DesignerProperties.IsDesignMode = false;
+        }
     }
 }
