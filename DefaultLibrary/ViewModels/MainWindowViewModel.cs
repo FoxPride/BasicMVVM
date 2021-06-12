815576058dbb543dd454485f24a635ceca5ce28f
andbayd <andrew@monosnap.com>
2021-06-12 07:06:37 +0300

    Base usage of mvvm

---
+++ DefaultLibrary/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace DefaultLibrary.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+            ChangeTitleCommand = new RelayCommand<string>(UpdateTitle);
+            CloseWindowCommand = new RelayCommand(CloseWindow);
+        }
+
+        #region Title : string - Window title
+
+        private string _title = "Test title";
+
+        /// <summary>Window title</summary>
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        #endregion
+
+        #region Commands
+        public ICommand ChangeTitleCommand { get; }
+
+        public ICommand CloseWindowCommand { get; }
+
+        #endregion
+
+        #region Methods
+
+        private void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+
+        private void CloseWindow()
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+    }
+}
