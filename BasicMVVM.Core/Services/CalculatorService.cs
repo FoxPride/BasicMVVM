e3bd0581eae13f861007ca88716bd6797536278e
FoxPride <c1ee3517@opayq.com>
2021-11-06 23:02:00 +0300

    xUnit test example

---
+++ BasicMVVM.Core/Services/CalculatorService.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicMVVM.Core.Services
+{
+    public class CalculatorService
+    {
+        public double Add(double variavel1, double variavel2)
+        {
+            return variavel1 + variavel2;
+        }
+        public double Multiply(double variavel1, double variavel2)
+        {
+            return variavel1 * variavel2;
+        }
+        public double Division(double variavel1, double variavel2)
+        {
+            return variavel1 / variavel2;
+        }
+        public double Subtract(double variavel1, double variavel2)
+        {
+            return variavel1 / variavel2;
+        }
+    }
+}
